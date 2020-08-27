886a724fadd0b2c2ded92fbcaef140473d9a20a7
adityayadav800 <cooladityaamo@gmail.com>
2020-08-27 13:52:42 +0530

    Minor Improvements

--- spiders/cloner.py
+++ spiders/cloner.py
@@ -23,55 +23,89 @@ class QuotesSpider(scrapy.Spider):
             for url in ejsLinks:
                 if(not ('https' in url)):
                     it=[]
+                    addr=url
                     for i in range(len(temp1)-1):
                         it.append(temp1[i])
-                    print(it,"+++++++++++++++++++++++++")
+                    #print(it,"+++++++++++++++++++++++++")
                     imdt='/'.join(it)
                     url=imdt+'/'+url
-                    print(url,"+++++++++")
-
-                res=requests.get(url)
-                x=url.split('/',3)
-                y=x[3].split('/')
-                print(y)
-                s=''
-                for i in range(len(y)-1):
-                    #print(y[i])
-                    if(i==0):
-                        s=s+y[i]
-                    else:
-                        s=s+'/'+y[i]
-                print(s)
-                if(not os.path.isdir(s)):
-                    os.makedirs(s)
-                open(x[3],'wb').write(res.content)
+                    #print(url,"+++++++++")
+                    res=requests.get(url)
+                    x=url.split('/')
+                    y=addr.split('/')
+                    #print(y)
+                    s=''
+                    for i in range(len(y)-1):
+                        #print(y[i])
+                        if(i==0):
+                            s=s+y[i]                                                                                                                                
+                        else:
+                            s=s+'/'+y[i]
+                    #print(s)
+                    if(not os.path.isdir(s)):
+                            os.makedirs(s)
+                    open(addr,'wb').write(res.content)
+                else:
+                    res=requests.get(url)
+                    x=url.split('/',3)
+                    y=x[3].split('/')
+                    #print(y)
+                    s=''
+                    for i in range(len(y)-1):
+                        #print(y[i])
+                        if(i==0):
+                            s=s+y[i]
+                        else:
+                            s=s+'/'+y[i]
+                    print(s)
+                    if(not os.path.isdir(s)):
+                        os.makedirs(s)
+                    open(x[3],'wb').write(res.content)
             ecss=response.xpath('//link[@rel=\'stylesheet\' or  @type=\'text/css\']/@href').extract()
             print(ecss)
             for url in ecss:
                 print(url)
                 if(not ('https' in url)):
                     it=[]
+                    addr=url
                     for i in range(len(temp1)-1):
                         it.append(temp1[i])
-                    print(it,"+++++++++++++++++++++++++")
+                    #print(it,"+++++++++++++++++++++++++")
                     imdt='/'.join(it)
                     url=imdt+'/'+url
-                    print(url,"+++++++++")
-                res=requests.get(url)
-                x=url.split('/',3)
-                y=x[3].split('/')
-                print(y)
-                s=''
-                for i in range(len(y)-1):
-                    #print(y[i])
-                    if(i==0):
-                        s=s+y[i]
-                    else:
-                        s=s+'/'+y[i]
-                print(s)
-                if(not os.path.isdir(s)):
-                    os.makedirs(s)
-                open(x[3],'wb').write(res.content)
+                    #print(url,"+++++++++")
+                    res=requests.get(url)
+                    x=url.split('/')
+                    y=addr.split('/')
+                    print(y,"*********")
+                    s=''
+                    for i in range(len(y)-1):
+                        #print(y[i])
+                        if(i==0):
+                            s=s+y[i]
+                        else:
+                            s=s+'/'+y[i]
+                    print(s,"++++++++++++++")
+                    if(not os.path.isdir(s)):
+                        os.makedirs(s)
+                    open(addr,'wb').write(res.content)
+                else:
+                    res=requests.get(url)
+                    x=url.split('/',3)
+                    y=x[3].split('/')
+                    print(y)
+                    s=''
+                    for i in range(len(y)-1):
+                        #print(y[i])
+                        if(i==0):
+                            s=s+y[i]
+                        else:
+                            s=s+'/'+y[i]
+                            print(s)
+                    if(not os.path.isdir(s)):
+                        os.makedirs(s)
+                    open(x[3],'wb').write(res.content)
+
             icss=response.xpath('//style[@type=\'text/css\']/text()').extract()
             ind=open('index.html','w')
             ind.write(response.text)
